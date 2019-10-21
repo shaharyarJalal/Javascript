487c9c96c0f679ac5833fb2b6ca9697ec060a3c0
shaharyarJalal <56484274+shaharyarJalal@users.noreply.github.com>
2019-10-21 13:18:37 +0500

    Javascript file

---
+++ demo.js
@@ -0,0 +1,19 @@
+// declaring a variable
+var x = 20;
+var y = 20;
+var sum = x+y;
+var name = "XYZ";
+
+// To write on web page
+document.writeln(x);
+document.writeln(name);
+
+// alert box
+alert(x);
+
+// concatenation
+
+document.writeln("my name is: " + name);
+document.writeln("value of x is: " + x);
+
+alert("sum of x and y is: " + sum);
